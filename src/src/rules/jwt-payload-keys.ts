5b0c1ff38cbdc3039dbb3fe2a6fa02098d0461bc
inkz <inkz@xakep.ru>
2019-10-12 18:26:10 +1000

    Update jwt-payload-keys rule

--- src/src/rules/jwt-payload-keys.ts
+++ src/src/rules/jwt-payload-keys.ts
@@ -11,28 +11,64 @@ function isJWT(ty: stadt.Type): boolean {
     || (packageName === '@panva/jose' && ['JWT', 'JWS'].includes(name));
 }
 
+function findSecret(arg: estree.Node, context: Context): void {
+  const secretType = getType(arg);
+  if (secretType && secretType.isString()) {
+    context.report({
+      node: arg,
+      checkId: 'jwt-secret',
+      extra: {
+        secret: (secretType as stadt.LiteralType).value
+      }
+    });
+  }
+}
+
 export const rule: Rule = {
   create(context: Context) {
     return {
       CallExpression(node: estree.CallExpression) {
         if (node.callee && (node.callee as estree.MemberExpression).object) {
           const obj: estree.MemberExpression = (node.callee as estree.MemberExpression);
-          if (obj.property && (obj.property as estree.Identifier).name === 'sign') {
-            const ty: stadt.Type | undefined = getType(obj.object);
-            if (ty && possibleTypes(ty).some(isJWT)) {
-              const arg = node.arguments[0];
-              const argType = getType(arg);
-              if (argType && argType.isObject()) {
-                const keys = argType.properties.keys();
-                for (let key of keys) {
+          const ty: stadt.Type | undefined = getType(obj.object);
+          if (ty && possibleTypes(ty).some(isJWT)) {
+            if (obj.property && (obj.property as estree.Identifier).name === 'decode') {
+              context.report({
+                node,
+                checkId: 'jwt-decode'
+              });           
+            }
+            if (obj.property && (obj.property as estree.Identifier).name === 'verify') {
+              const [, secret, opts] = node.arguments;
+              secret && findSecret(secret, context);
+              if (opts) {
+                const optsType = getType(opts);
+                if (optsType && optsType.isPrimitive()) {
                   context.report({
                     node,
-                    checkId: 'jwt-payload-key',
-                    extra: {
-                      key
-                    }
+                    checkId: 'jwt-opts-primitive'
                   });
                 }
+              }
+            }
+            if (obj.property && (obj.property as estree.Identifier).name === 'sign') {
+              const ty: stadt.Type | undefined = getType(obj.object);
+              const [payload, secret, ] = node.arguments;
+              secret && findSecret(secret, context);
+              const argType = getType(payload);
+              if (argType && argType.isObject()) {
+                const keysMap = argType.properties.keys();
+                const keys = [];
+                for (let key of keysMap) {
+                  keys.push(key);
+                }
+                context.report({
+                  node,
+                  checkId: 'jwt-payload-key',
+                  extra: {
+                    keys
+                  }
+                });
               }
             }
           }
